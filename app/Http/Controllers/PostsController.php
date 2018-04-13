492aac6f9dca285664f7913e2047bd90087d56c8
Ahmed-Te7a <ahmedabdelftah95165@gmail.com>
2018-04-13 13:59:03 +0200

    Add layouts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,11 +9,16 @@ class PostsController extends Controller
     public function index()
     {
         $posts = Post::all();
-        
+
         return view('posts.index',[
 
             'posts' => $posts
         ]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
     }
 
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,16 +1,7 @@
-<html>
-<head>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+@extends('layouts.master')
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-</head>
-<body>
+@section('content')
 
 <h1>Posts Index</h1>
 <ul>
@@ -25,6 +16,5 @@
 
 
 </ul>
-</body>
 
-</html>
+@endsection
