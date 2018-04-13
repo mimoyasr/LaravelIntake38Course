3f2d382adb75d0072f3dbb458f36b6948bc2a93d
Ahmed-Te7a <ahmedabdelftah95165@gmail.com>
2018-04-13 10:11:21 +0200

    Add psots

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('posts',function(){
+    return 'hello posts';
+});
